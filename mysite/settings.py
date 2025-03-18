c0a54d0432df083abecf7d8c014eb46dd0bc55ac
grace.vivian <grace.vivian@travelperk.com>
2025-03-18 14:31:25 +0100

    Update settings to configure URL

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jtebl9o*is14wy0hj3gun&-tk-zd^+mf$@s2e5gxdsfc$atw@*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
