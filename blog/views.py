583d222b7441a55dffdacafb7f3da7336c0178d0
grace.vivian <grace.vivian@travelperk.com>
2025-03-18 16:00:41 +0100

    Modify template to post from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+# . before models means current directory or current application
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
